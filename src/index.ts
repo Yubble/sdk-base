bcc15ad400807e4e17e22b069f793d06b3782620
Yubble <hailen91@qq.com>
2022-06-12 17:44:45 +0800

    更新

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,16 @@
  * @Author: 刘燕保
  * @Date: 2021-10-31 12:07:29
  */
-import { module1, getEvenNo } from './module1'
+import { module1, getEvenNo } from "./module1"
 
-const namelog = void module1('Yubble')
+const namelog = void module1("Yubble")
 
 const evenNo = getEvenNo([3, 5, 12, 9, 17, 28, 4])
 
 namelog()
 
-const obj = { name: 'Yubble' }
+const obj = { name: "Yubble" }
 
-console.log('找到 obj 中的name is ', Reflect.get(obj, 'name'))
+console.log('找到 obj 中的name is ', Reflect.get(obj, "name"))
 
-console.log('执行数组找偶, res is ', evenNo)
+console.log("执行数组找偶, res is ", evenNo)
